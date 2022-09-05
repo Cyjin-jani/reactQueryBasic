659f68030f9be7c25a5fd727ed374b2eaa49a15b
yjcho <jinjanic91@gmail.com>
2022-09-05 23:25:22 +0900

    feat. 2-11 done

--- src/PostDetail.jsx
+++ src/PostDetail.jsx
@@ -1,41 +1,79 @@
+import { useQuery } from 'react-query'
+
 async function fetchComments(postId) {
   const response = await fetch(
     `https://jsonplaceholder.typicode.com/comments?postId=${postId}`
-  );
-  return response.json();
+  )
+  return response.json()
 }
 
 async function deletePost(postId) {
   const response = await fetch(
     `https://jsonplaceholder.typicode.com/postId/${postId}`,
-    { method: "DELETE" }
-  );
-  return response.json();
+    { method: 'DELETE' }
+  )
+  return response.json()
 }
 
 async function updatePost(postId) {
   const response = await fetch(
     `https://jsonplaceholder.typicode.com/postId/${postId}`,
-    { method: "PATCH", data: { title: "REACT QUERY FOREVER!!!!" } }
-  );
-  return response.json();
+    { method: 'PATCH', data: { title: 'REACT QUERY FOREVER!!!!' } }
+  )
+  return response.json()
 }
 
 export function PostDetail({ post }) {
-  // replace with useQuery
-  const data = [];
+  // 쿼리 키 사용의 틀린 예시
+  // const { data, isError, error, isLoading } = useQuery(
+  //   'postDetail',
+  //   () => fetchComments(post.id)
+  // )
+  /**
+   * query key가 위와 같은 경우,comments가 refetch되지 않음
+   * WHY??
+   * 모든 query가 같은 쿼리를 가지고 있기 때문.
+   * 따라서 useQuery에서는 배열로 받음 => deps와 비슷한 로직.
+   * 배열의 값들이 모두 같지 않다면 다른 key로 판단하여 refetch!
+   *
+   * Data for queries with known keys only refetched upon trigger
+   * examples of tirggers:
+   * - component remount
+   * - window refocus
+   * - running refetch function
+   * - automated refetch
+   * - query invalidation after a mutation
+   *
+   * */
+  const { data, isError, error, isLoading } = useQuery(
+    ['postDetail', post.id],
+    () => fetchComments(post.id)
+  )
+
+  if (isError) {
+    return (
+      <>
+        <h3>무언가 잘못되었다..</h3>
+        <p>{error.toString()}</p>
+      </>
+    )
+  }
+
+  if (isLoading) {
+    return <h3>로딩중....</h3>
+  }
 
   return (
     <>
-      <h3 style={{ color: "blue" }}>{post.title}</h3>
+      <h3 style={{ color: 'blue' }}>{post.title}</h3>
       <button>Delete</button> <button>Update title</button>
       <p>{post.body}</p>
       <h4>Comments</h4>
-      {data.map((comment) => (
+      {data.map(comment => (
         <li key={comment.id}>
           {comment.email}: {comment.body}
         </li>
       ))}
     </>
-  );
+  )
 }
--- src/Posts.jsx
+++ src/Posts.jsx
@@ -1,26 +1,65 @@
-import { useState } from "react";
+import { useState } from 'react'
+import { useQuery } from 'react-query'
 
-import { PostDetail } from "./PostDetail";
-const maxPostPage = 10;
+import { PostDetail } from './PostDetail'
+const maxPostPage = 10
 
 async function fetchPosts() {
+  // 에러를 던지기 위한 잘못된 url
+  // const response = await fetch(
+  //   'https://jsonplaceholderㅇㅂㅈㅇㅂㅈㅇㅂㅈㄴ.typicode.com/posts?_limit=10&_page=0'
+  // )
+
+  // 정상 url
   const response = await fetch(
-    "https://jsonplaceholder.typicode.com/posts?_limit=10&_page=0"
-  );
-  return response.json();
+    'https://jsonplaceholder.typicode.com/posts?_limit=10&_page=0'
+  )
+  return response.json()
 }
 
 export function Posts() {
-  const [currentPage, setCurrentPage] = useState(0);
-  const [selectedPost, setSelectedPost] = useState(null);
+  const [currentPage, setCurrentPage] = useState(0)
+  const [selectedPost, setSelectedPost] = useState(null)
 
-  // replace with useQuery
-  const data = [];
+  /**
+   * StaleTime is for re-fetching
+   *
+   * Cache is for data that might be re-used later
+   *   - query goes into 'cold storage' if there's no active useQuery
+   *   - cache data expires after cacheTime (default: 5min)
+   *     = how long it's been since the last active useQuery (useQuery가 active되고 나서 얼마나 지났는지)
+   *   - after the cache expires, the data is garbage collected
+   *
+   * Cache is backup data to display while fetching
+   *
+   */
+  const { data, isError, error, isLoading } = useQuery('posts', fetchPosts, {
+    // staleTime: 1000,
+  })
+  /**
+   * isLoading과 isFetching의 차이는??
+   * isFetching은 async query function이 아직 resolved 되지 않은 상태를 말함
+   * isLoading은 cached data도 없고, 거기에 + isFetching인 상태이다
+   */
+  if (isLoading) {
+    return <h3>Loading...</h3>
+  }
+  // react-query는 기본적으로 3번 시도한 뒤 최종적으로 실패라고 결론 짓는다.
+  // 따라서 현재 코드의 경우 처음에 Loading이 화면에 나타나고, (3번의 시도 + 실패한 뒤)
+  // isError라고 판단되어 아래 잘못되었다는 글자가 화면에 표시된다.
+  if (isError) {
+    return (
+      <>
+        <h3>무언가 잘못 됨...</h3>
+        <p>{error.toString()}</p>
+      </>
+    )
+  }
 
   return (
     <>
       <ul>
-        {data.map((post) => (
+        {data.map(post => (
           <li
             key={post.id}
             className="post-title"
@@ -42,5 +81,5 @@ export function Posts() {
       <hr />
       {selectedPost && <PostDetail post={selectedPost} />}
     </>
-  );
+  )
 }
