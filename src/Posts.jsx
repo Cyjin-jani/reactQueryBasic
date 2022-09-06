ae2ddf6b863612a118f14bda7f20a0a6463284bc
yjcho <jinjanic91@gmail.com>
2022-09-06 23:45:04 +0900

    feat. pagination and prefetching

--- src/Posts.jsx
+++ src/Posts.jsx
@@ -1,10 +1,10 @@
-import { useState } from 'react'
-import { useQuery } from 'react-query'
+import { useEffect, useState } from 'react'
+import { useQuery, useQueryClient } from 'react-query'
 
 import { PostDetail } from './PostDetail'
 const maxPostPage = 10
 
-async function fetchPosts() {
+async function fetchPosts(pageNum) {
   // 에러를 던지기 위한 잘못된 url
   // const response = await fetch(
   //   'https://jsonplaceholderㅇㅂㅈㅇㅂㅈㅇㅂㅈㄴ.typicode.com/posts?_limit=10&_page=0'
@@ -12,14 +12,32 @@ async function fetchPosts() {
 
   // 정상 url
   const response = await fetch(
-    'https://jsonplaceholder.typicode.com/posts?_limit=10&_page=0'
+    `https://jsonplaceholder.typicode.com/posts?_limit=10&_page=${pageNum}`
   )
   return response.json()
 }
 
 export function Posts() {
-  const [currentPage, setCurrentPage] = useState(0)
+  const [currentPage, setCurrentPage] = useState(1)
   const [selectedPost, setSelectedPost] = useState(null)
+
+  /** preFetching을 위한 처리
+   *  queryClient의 prefetchQuery메소드를 이용하여 데이터를 미리 가져올 수 있다
+   *
+   *  Prefetch
+   *  - adds data to cache
+   *  - automatically stale (configurable)
+   *  - shows while re-fetching (cache가 expired되지 않는 한)
+   *  - prefetch는 단지 pagination 뿐 아니라 사전에 필요한 (예측된) 데이터의 니즈가 있는 경우에 사용 가능하다
+   */
+  const queryClient = useQueryClient()
+  useEffect(() => {
+    if (currentPage < maxPostPage) {
+      // 마지막 페이지의 경우 다음 페이지가 존재하지 않으므로 미리 가져올 데이터가 없음
+      const nextPage = currentPage + 1
+      queryClient.prefetchQuery(['posts', nextPage], () => fetchPosts(nextPage))
+    }
+  }, [currentPage, queryClient])
 
   /**
    * StaleTime is for re-fetching
@@ -33,15 +51,27 @@ export function Posts() {
    * Cache is backup data to display while fetching
    *
    */
-  const { data, isError, error, isLoading } = useQuery('posts', fetchPosts, {
-    // staleTime: 1000,
-  })
+  const { data, isError, error, isLoading, isFetching } = useQuery(
+    ['posts', currentPage],
+    () => fetchPosts(currentPage),
+    {
+      // staleTime: 1000,
+      keepPreviousData: true, // 이전 페이지로 이동하더라도 이전의 데이터를 가지고 있어 바로 보여줄 수 있다
+    }
+  )
   /**
    * isLoading과 isFetching의 차이는??
    * isFetching은 async query function이 아직 resolved 되지 않은 상태를 말함
+   * (해당 상태인 경우에, 캐시데이터의 존재 여부와는 상관없이 query function 실행 중이면 true가 된다)
    * isLoading은 cached data도 없고, 거기에 + isFetching인 상태이다
    */
+
+  // if (isFetching) {
+  //   return <h3>isFetching...</h3>
+  // }
   if (isLoading) {
+    // pagination이 추가되어 미리 데이터를 가져오는 prefetch가 생긴 뒤로는
+    // 아래 로딩이 뜨지 않는다 왜냐하면 이미 prefetch가 되어 cached된 data가 존재하기 때문이다
     return <h3>Loading...</h3>
   }
   // react-query는 기본적으로 3번 시도한 뒤 최종적으로 실패라고 결론 짓는다.
@@ -70,11 +100,21 @@ export function Posts() {
         ))}
       </ul>
       <div className="pages">
-        <button disabled onClick={() => {}}>
+        <button
+          disabled={currentPage <= 1}
+          onClick={() => {
+            setCurrentPage(prev => prev - 1)
+          }}
+        >
           Previous page
         </button>
-        <span>Page {currentPage + 1}</span>
-        <button disabled onClick={() => {}}>
+        <span>Page {currentPage}</span>
+        <button
+          disabled={currentPage >= maxPostPage}
+          onClick={() => {
+            setCurrentPage(prev => prev + 1)
+          }}
+        >
           Next page
         </button>
       </div>
