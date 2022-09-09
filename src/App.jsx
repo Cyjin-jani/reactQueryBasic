ea0954e237fc67463ff4772e405660a55c1db477
yjcho <jinjanic91@gmail.com>
2022-09-09 18:56:27 +0900

    feat. infiteSroll init

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { Posts } from './Posts'
 import './App.css'
 import { QueryClient, QueryClientProvider } from 'react-query'
 import { ReactQueryDevtools } from 'react-query/devtools'
+import { InfinitePeople } from './people/InfinitePeople'
+import { InfiniteSpecies } from './species/InfiniteSpecies'
 
 const queryClient = new QueryClient()
 
@@ -10,10 +12,13 @@ function App() {
     // provide React Query client to App
     <QueryClientProvider client={queryClient}>
       <div className="App">
-        <h1>Blog Posts</h1>
-        <Posts />
+        {/* <h1>Blog Posts</h1>
+        <Posts /> */}
+        <h1>Infinite SWAPI</h1>
+        <InfinitePeople />
+        {/* <InfiniteSpecies /> */}
+        <ReactQueryDevtools />
       </div>
-      <ReactQueryDevtools />
     </QueryClientProvider>
   )
 }
